c15739af314fb67afd9dfde28be08f46a0acc3e5
Emiltv <44637266+Emiltv@users.noreply.github.com>
2023-02-02 10:03:01 +0100

    Add files via upload

---
+++ Assets/SimonMiniGame/Scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "HighScore: " + PlayerPrefs.GetInt("HiScore");
+    }
+
+    
+}
